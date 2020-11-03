ca14d9465a2b99666f456bc0707abcc1135f6f14
Luis Gustavo <luisgbyte>
2020-11-03 10:54:43 -0300

    first commit

---
+++ pokemon.py
@@ -0,0 +1,21 @@
+class Pokemon:
+    def __init__(self, tipo, especie, level=1, nome=None):
+        self.tipo = tipo
+        self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+
+    def __str__(self):
+        return "{}({})".format(self.nome, self.level)
+
+    def atacar(self, pokemon):
+        print("{} atacou {}!".format(self, pokemon))
+
+
+meu_pokemon = Pokemon("fogo", "charmander", nome="L", level=50)
+
+print(meu_pokemon)
