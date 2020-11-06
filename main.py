ae6416b4667d405635fdbf9bc50f2aacbb0a3876
Luis Gustavo <luisgbyte>
2020-11-06 18:50:15 -0300

    salvando o game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+
 from pokemon import *
 from pessoa import *
 
@@ -30,34 +32,59 @@ def escolher_pokemon_inicial(player):
             print("Escolha inválida!")
 
 
+def salvar_jogo(player):
+    try:
+        with open("database.db", "wb") as arquivo:
+            pickle.dump(player, arquivo)
+            print("Jogo salvo com sucesso!")
+    except Exception as error:
+        print("Erro ao salvar jogo")
+        print(error)
+
+def carregar_jogo():
+    try:
+        with open("database.db", "rb") as arquivo:
+            player = pickle.load(arquivo)
+            print("Loading feito com sucesso")
+            return player
+    except Exception as error:
+        print("Save não encontrado!!!")
+ 
+
+
 if __name__=="__main__":
     print('------------------------------------------')
     print("Bem vindo ao game Pokemon RPG de terminal")
     print('------------------------------------------')
 
-    nome = input("Olá, qual é o seu nome: ")
-    player = Player(nome)
+    player = carregar_jogo()
 
-    print("Olá {}, esse é um mundo habitado por pokemons, sua missão é se tornar um grande mestre pokemon".format(player))
-    print("Capture o máximo de pokemons que conseguir e lute com seus inimigos")
-    player.mostrar_dinheiro()
+    if not player:
+        nome = input("Olá, qual é o seu nome: ")
+        player = Player(nome)
 
-    if player.pokemons:
-        print("Já vi que você têm alguns pokemons")
-        player.mostrar_pokemons()
-    else:
-        print("Você não têm nenhum pokemon, portando precisa escolher um")
-        escolher_pokemon_inicial(player)
+        print("Olá {}, esse é um mundo habitado por pokemons, sua missão é se tornar um grande mestre pokemon".format(player))
+        print("Capture o máximo de pokemons que conseguir e lute com seus inimigos")
+        player.mostrar_dinheiro()
 
-    print("Pronto, agora que você já possui um pokemon, enfrente seu arqui-rival Gary desde o jardim de infância")
-    gary = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squirtle", level=1)])
-    player.batalhar(gary)
+        if player.pokemons:
+            print("Já vi que você têm alguns pokemons")
+            player.mostrar_pokemons()
+        else:
+            print("Você não têm nenhum pokemon, portando precisa escolher um")
+            escolher_pokemon_inicial(player)
+
+        print("Pronto, agora que você já possui um pokemon, enfrente seu arqui-rival Gary desde o jardim de infância")
+        gary = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squirtle", level=1)])
+        player.batalhar(gary)
+        salvar_jogo(player)
 
     while True:
         print("----------------------------------")
         print("O que deseja fazer?")
         print("1 - Explorar o mapa")
         print("2 - Lutar com um inimigo")
+        print("3 - Ver Pokeagenda")
         print("0 - Sair do jogo")
         escolha = input("Sua escolha:")
 
@@ -66,9 +93,13 @@ if __name__=="__main__":
             break
         elif escolha =="1":
             player.explorar()
+            salvar_jogo(player)
         elif escolha =="2":
             inimigo_aleatorio =Inimigo()
             player.batalhar(inimigo_aleatorio)
+            salvar_jogo
+        elif escolha =="3":
+            player.mostrar_pokemons()
         else:
             print("Escolha inválida")
          
--- pessoa.py
+++ pessoa.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import random
 
 from pokemon import *
@@ -127,9 +128,12 @@ class Player(Pessoa):
 class Inimigo(Pessoa):
     tipo = "inimigo"
 
-    def __init__(self, nome=None, pokemons=[]):
+    def __init__(self, nome=None, pokemons=None):
         if not pokemons:
+            pokemons_aleatorios = []
             for i in range(random.randint(1, 6)):
-                pokemons.append(random.choice(POKEMONS))
+                 pokemons_aleatorios.append(random.choice(POKEMONS))
             
-        super().__init__(nome=nome, pokemons=pokemons)
+            super().__init__(nome=nome, pokemons=pokemons_aleatorios)
+        else:
+            super().__init__(nome=nome, pokemons=pokemons)
